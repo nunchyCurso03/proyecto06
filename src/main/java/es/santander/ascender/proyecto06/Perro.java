1c1605c77640b3b348e3760e894cefda6eeaed29
curso00 <curso00@nomail.com>
2024-11-14 13:52:09 +0100

    Ejemplo de clase con datos

---
+++ src/main/java/es/santander/ascender/proyecto06/Perro.java
@@ -0,0 +1,23 @@
+package es.santander.ascender.proyecto06;
+
+public class Perro {
+
+    private double peso;
+ 
+    public Perro(double peso) {
+        // Que pasa en peso 0 o negativo
+        this.peso = peso;
+    }
+
+    public void comer(double pesoComida) {
+        this.peso += pesoComida;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }    
+}
